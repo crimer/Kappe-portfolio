1e58a944dde2a9b2e42cd5a276a948a1bd30b55b
crimer <99nik.pro@gmail.com>
2019-03-12 22:44:28 +1000

    ready homepage

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+
+	document.querySelector('#btn_info').addEventListener('click',function  () {
+		document.querySelector('#btn_info').classList.toggle('open');
+	});
+
+
+
+}
---
+++ build/js/app.js
@@ -0,0 +1,2 @@
+"use strict";window.onload=function(){document.querySelector("#btn_info").addEventListener("click",function(){document.querySelector("#btn_info").classList.toggle("open")})};
+//# sourceMappingURL=app.js.map
